f9791bdd94ff2f491f7096056ecf0d3cb1a91f7d
MartijnSlot <martijn.slot@nedap.com>
2020-09-21 15:31:15 +0200

    added run function

--- src/main/kotlin/Bonakkie.kt
+++ src/main/kotlin/Bonakkie.kt
@@ -1,9 +1,13 @@
 class Bonakkie {
 
+    fun run(args: Array<String>){
+        println("BONAK APP v1.")
+    }
+
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println("bladiebla")
+            Bonakkie().run(args)
         }
     }
 }
